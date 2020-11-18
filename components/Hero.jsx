9fbd584b8babc4c387dd595eefd43b3a69f307aa
iamsahebgiri <iamsahebgiri@gmail.com>
2020-11-18 23:32:26 +0530

    Added Day 03 Smartwatch JS

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -14,13 +14,16 @@ export default function Hero() {
         </p>
       </div>
       <div className='flex mt-10 justify-center'>
-        <Link href='/explore'>
-          <a className='rounded-md px-4 md:px-5 xl:px-4 py-3 md:py-4 xl:py-3 bg-primary-1 hover:bg-primary-2 md:text-lg xl:text-base text-white font-semibold leading-tight shadow-md'>
-            Watch Videos
-          </a>
-        </Link>
         <a
-          href='#contribute'
+          href='https://www.youtube.com/channel/UC9VTm-aiiHa4uUMWAgYtcHg'
+          target='_blank'
+          className='rounded-md px-4 md:px-5 xl:px-4 py-3 md:py-4 xl:py-3 bg-primary-1 hover:bg-primary-2 md:text-lg xl:text-base text-white font-semibold leading-tight shadow-md'
+        >
+          Watch Videos
+        </a>
+        <a
+          href='https://github.com/iamsahebgiri/30days30projects/tree/main/public/project'
+          target='_blank'
           className='ml-4 rounded-md px-4 md:px-5 xl:px-4 py-3 md:py-4 xl:py-3 bg-white hover:bg-gray-200 md:text-lg xl:text-base text-gray-800 font-semibold leading-tight shadow-md'
         >
           Source Code
---
+++ public/project/smartwatch-js/app.js
@@ -0,0 +1,27 @@
+const clockElement = document.querySelector('.clock');
+const extraInfoElement = document.querySelector('.extra-info');
+const clockBorderElement = document.querySelector('.clock-border');
+
+// helper functions
+const format = (str) => str.toString().padStart(2, '0');
+
+const dateFn = () => {
+  const dt = new Date();
+  const hr = dt.getHours();
+  const min = dt.getMinutes();
+
+  clockElement.textContent = `${format(hr)}:${format(min)}`;
+  extraInfoElement.textContent = luxon.DateTime.local().toFormat(
+    'cccc, MMM dd',
+  );
+};
+
+dateFn();
+setInterval(dateFn, 1000);
+
+let deg = 0;
+// animation spin
+setInterval(() => {
+  clockBorderElement.style.background = `linear-gradient(${deg}deg,  #ffb703, #fb8500)`;
+  deg = (deg + 1) % 360;
+}, 1);
